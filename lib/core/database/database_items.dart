afed73e27aa22cd137b8c27ad11c32b9d00c616d
deadlyunicorn <petrache.dev@gmail.com>
2024-05-15 16:20:07 +0300

    added a lastModified field on DatabasePage and a copyWith method on Note class.

--- lib/core/database/database_items.dart
+++ lib/core/database/database_items.dart
@@ -4,15 +4,25 @@ part "database_items.g.dart";
 
 @collection
 class Note {
-  Id id = Isar.autoIncrement; // you can also use id = null to auto increment
+  Id id; // you can also use id = null to auto increment
 
   final String textContent;
   final DateTime modifiedDate;
 
-  Note({
-    required this.textContent,
-    required this.modifiedDate,
-  });
+  Note({required this.textContent, required this.modifiedDate, Id? id})
+      : id = id ?? Isar.autoIncrement;
+
+  Note copyWith({
+    Id? id,
+    String? textContent,
+    DateTime? modifiedDate,
+  }) {
+    return Note(
+      textContent: textContent ?? this.textContent,
+      modifiedDate: modifiedDate ?? this.modifiedDate,
+      id: id ?? this.id,
+    );
+  }
 }
 
 @collection
@@ -23,14 +33,23 @@ class DatabasePage {
   //* This should keep track of the last 30 snapshots.
   //? Snapshots store the ids of the Notes belonging to the page.
   //? must be sorted by date.
-  final List<int> autoSnapshots;
+
+  List<int> _autoSnapshots;
+  List<int> get autoSnapshots => _autoSnapshots;
+  void setAutoSnapshots(List<int> newState) {
+    lastModified = DateTime.now();
+    _autoSnapshots = newState;
+  }
+
   //* This should keep track of snapshots taken by the user ( not-limited ).
-  final List<int> manualSnapshots;
+  List<int> manualSnapshots;
+
+  DateTime lastModified = DateTime(1970);
 
   DatabasePage({
-    required this.autoSnapshots,
+    required List<int> autoSnapshots,
     required this.manualSnapshots,
-  });
+  }) : _autoSnapshots = autoSnapshots;
 
   //TODO selecting an older auto snapshot should not delete the other ones until you press "RESTORE TO THIS POINT"
   //? It should work as a preview. Definitely don't delete the manual snapshots.
