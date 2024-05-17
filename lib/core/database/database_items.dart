bd321ebcffedfe2f997f02aff718519be710e169
deadlyunicorn <petrache.dev@gmail.com>
2024-05-17 03:13:35 +0300

    Pages are now simply Notes that are grouped by an imaginary pageId.

--- lib/core/database/database_items.dart
+++ lib/core/database/database_items.dart
@@ -4,66 +4,35 @@ part "database_items.g.dart";
 
 @collection
 class Note {
-  Id id; // you can also use id = null to auto increment
+  final Id id; // you can also use id = null to auto increment
 
   final String textContent;
   final DateTime modifiedDate;
 
-  Note({required this.textContent, required this.modifiedDate, Id? id})
-      : id = id ?? Isar.autoIncrement;
+  final bool wasAutoSaved;
+  final int pageId;
+
+  Note({
+    required this.textContent,
+    required this.modifiedDate,
+    required this.wasAutoSaved,
+    required this.pageId,
+    this.id = Isar.autoIncrement,
+  });
 
   Note copyWith({
-    Id? id,
     String? textContent,
     DateTime? modifiedDate,
+    bool? wasAutoSaved,
+    int? pageId,
+    int? id,
   }) {
     return Note(
       textContent: textContent ?? this.textContent,
       modifiedDate: modifiedDate ?? this.modifiedDate,
+      wasAutoSaved: wasAutoSaved ?? this.wasAutoSaved,
+      pageId: pageId ?? this.pageId,
       id: id ?? this.id,
     );
   }
-}
-
-@collection
-class DatabasePage {
-  // The id of the page.
-  Id id = Isar.autoIncrement;
-
-  //* This should keep track of the last 30 snapshots.
-  //? Snapshots store the ids of the Notes belonging to the page.
-  //? must be sorted by date.
-
-  List<int> _autoSnapshots;
-  List<int> get autoSnapshots => _autoSnapshots;
-  void setAutoSnapshots(List<int> newState) {
-    lastModified = DateTime.now();
-    _autoSnapshots = newState;
-  }
-
-  //* This should keep track of snapshots taken by the user ( not-limited ).
-  List<int> manualSnapshots;
-
-  DateTime lastModified = DateTime(1970);
-
-  DatabasePage({
-    required List<int> autoSnapshots,
-    required this.manualSnapshots,
-  }) : _autoSnapshots = autoSnapshots;
-
-  //TODO selecting an older auto snapshot should not delete the other ones until you press "RESTORE TO THIS POINT"
-  //? It should work as a preview. Definitely don't delete the manual snapshots.
-}
-
-class CompletePage {
-  CompletePage({
-    required this.currentPage,
-    required this.currentNote,
-  });
-
-  //? snapshots are stored here
-  DatabasePage currentPage;
-
-  //? our current state
-  Note currentNote;
 }
